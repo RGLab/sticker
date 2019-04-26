1049ab253c8d313dd9bbc01c57dbf007b49ae3b9
mikejiang <wjiang2@fhcrc.org>
2019-04-26 16:59:35 -0700

    add logos for ggcyto and opencyto

---
+++ ggcyto/logo_gen.R
@@ -0,0 +1,8 @@
+hexSticker::sticker(package = "ggcyto", filename = "ggcyto/logo.png"
+                    , subplot = "ggcyto/icons8-scatter-plot-96.png"
+                    # , s_width = 0.65
+                    , s_height = 0.4 , s_x = 1, s_y = 0.7
+                    # , spotlight =T
+                    , h_fill = "white", h_color = "#3498DB"
+                    , p_size = 25, p_color = "#3498DB"
+                    )
---
+++ opencyto/logo_gen.R
@@ -0,0 +1,9 @@
+col <- "#1ABC9C"
+hexSticker::sticker(package = "openCyto", filename = "opencyto/logo.png"
+                    , subplot = "opencyto/icons8-automatic-100.png"
+                    # , s_width = 0.65
+                    , s_height = 0.3 , s_x = 1, s_y = 0.7
+                    # , spotlight =T
+                    , h_fill = "white", h_color = col
+                    , p_size = 25, p_color = col
+)
