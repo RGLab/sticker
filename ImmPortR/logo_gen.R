55ae186377801c4039937869c907f75557ee7956
Ju Yeong Kim <jkim2345@fredhutch.org>
2019-04-29 10:37:58 -0700

    Save as logo.png

--- ImmPortR/logo_gen.R
+++ ImmPortR/logo_gen.R
@@ -3,7 +3,7 @@ hexSticker::sticker(
   package = "ImmPortR", p_family = "serif", p_y = 1.35,
   h_fill = "#222222", h_color = "#53309E", h_size = 1.4,
   s_x = 1, s_y = 0.75, s_width = 0.35, s_height = 0.35,
-  filename = "ImmPortR/ImmPortR.png"
+  filename = "ImmPortR/logo.png"
 )
 
 # hexSticker::sticker(
@@ -11,5 +11,5 @@ hexSticker::sticker(
 #   package = "ImmPortR", p_family = "serif", p_y = 1.35,
 #   h_fill = "#222222", h_color = "#53309E", h_size = 1.4,
 #   s_x = 1, s_y = 0.75,
-#   filename = "ImmPortR/ImmPortR_logo.png"
+#   filename = "ImmPortR/logo_immport.png"
 # )
